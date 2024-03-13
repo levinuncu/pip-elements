3b5a981f4a407c256fce8f3f52c99dea71c34859
Levin Uncu <levin@uncu.de>
2024-03-13 15:47:05 +0100

    fix

--- src/Controller/ContentElement/ContentBoxStart.php
+++ src/Controller/ContentElement/ContentBoxStart.php
@@ -13,28 +13,37 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * @ContentElement("content_box_start", category="content_box")
+ * @ContentElement("content_box_start", category="content_box", template="content_element/content_box_start.html.twig")
  */
 class ContentBoxStart extends AbstractContentElementController
 {
-  protected function getResponse(Template $template, ContentModel $model, Request $request): Response
-  {
-    if ($this->container->get('contao.routing.scope_matcher')->isBackendRequest($request)) {
-      $template = new BackendTemplate('be_wildcard');
-      return $template->getResponse();
-    }
+    protected function getResponse(
+        Template $template,
+        ContentModel $model,
+        Request $request
+    ): Response {
+        if (
+            $this->container
+                ->get("contao.routing.scope_matcher")
+                ->isBackendRequest($request)
+        ) {
+            $template = new BackendTemplate("be_wildcard");
+            return $template->getResponse();
+        }
 
-    if (!empty($model->content_box_background_color_custom)) {
-      $template->backgroundColor = "--bc: #".$model->content_box_background_color_custom;
-    } else {
-      $template->backgroundColor = "--bc: #".$model->content_box_background_color;
-    }
+        if (!empty($model->content_box_background_color_custom)) {
+            $template->backgroundColor =
+                "--bc: #" . $model->content_box_background_color_custom;
+        } else {
+            $template->backgroundColor =
+                "--bc: #" . $model->content_box_background_color;
+        }
 
-    if ($model->addContentBoxImage && !empty($model->singleSRC)) {
-      $file = FilesModel::findByUuid($model->singleSRC);
+        if ($model->addContentBoxImage && !empty($model->singleSRC)) {
+            $file = FilesModel::findByUuid($model->singleSRC);
 
-      $template->backgroundImagePath = $file->path;
+            $template->backgroundImagePath = $file->path;
+        }
+        return $template->getResponse();
     }
-    return $template->getResponse();
-  }
 }
